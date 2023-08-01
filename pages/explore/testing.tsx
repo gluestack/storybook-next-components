729b8718672b799c1036c1b58d84f20370f1b3b0
mayank-96 <pagarmayank07@gmail.com>
2023-08-01 15:43:21 +0530

    feat: test style changes

---
+++ pages/explore/testing.tsx
@@ -0,0 +1,13 @@
+import { Box, Switch } from '@/components';
+import React from 'react';
+
+function Testing() {
+  return (
+    <Box>
+      <Switch value={true} />
+      <Switch value={false} />
+    </Box>
+  );
+}
+
+export default Testing;
