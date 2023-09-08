3a0b27ca6e09324c2fda8c2ba5ee176a474d887c
Rohit Singh <rohit99.ind@gmail.com>
2023-09-08 13:13:28 +0530

    feat: new design and meta data

--- pages/explore/[[...slug]].tsx
+++ pages/explore/[[...slug]].tsx
@@ -1,20 +1,32 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
-import path from 'path';
-import DirectoryTree from 'directory-tree';
-import { getFilePaths } from '../../utils';
-import React from 'react';
-import StoryData from '../../storybook-components-to-next.config';
-import { Center, VStack, Heading, Box, HStack } from '@gluestack-ui/themed';
-import { View } from 'react-native';
+import { GetStaticPaths, GetStaticProps } from "next";
+import path from "path";
+import DirectoryTree from "directory-tree";
+import { getFilePaths } from "../../utils";
+import React from "react";
+import StoryData from "../../storybook-components-to-next.config";
 import {
+  Center,
+  VStack,
+  Heading,
+  Box,
+  Text,
+  HStack,
+  Button,
+  Alert,
+  AlertIcon,
+  AlertText,
+  InfoIcon,
   GluestackUIProvider,
+} from "@gluestack-ui/themed";
+import { View } from "react-native";
+import {
   Toast,
   ToastTitle,
   ToastDescription,
   Pressable,
   Icon,
   CloseIcon,
-} from '@gluestack-ui/themed';
+} from "@gluestack-ui/themed";
 
 interface Option {
   control: string;
@@ -38,14 +50,14 @@ interface Combination {
 }
 
 const STATE_PROPERTIES = [
-  'isHovered',
-  'isPressed',
-  'isFocused',
-  'isFocusVisible',
-  'isDisabled',
-  'isInvalid',
-  'isReadonly',
-  'isRequired',
+  "isHovered",
+  "isPressed",
+  "isFocused",
+  "isFocusVisible",
+  "isDisabled",
+  "isInvalid",
+  "isReadonly",
+  "isRequired",
 ];
 
 function generateCombinations(
@@ -82,8 +94,32 @@ interface ExplorePageProps {
   slug: string;
 }
 
+const groupAllSortedCombinations = (
+  allCombinations: Array<any>,
+  key: string
+) => {
+  let allAvailableSize = {} as any;
+  if (allCombinations.length) {
+    allCombinations.map((item) => {
+      if (item[key]) {
+        allAvailableSize[item[key]] = 1;
+      }
+    });
+  }
+
+  let CombinationsSortedByKeys = {} as any;
+  Object.keys(allAvailableSize).map((combinationKey) => {
+    CombinationsSortedByKeys[combinationKey] = allCombinations.filter(
+      (item) => {
+        return item[key] === combinationKey;
+      }
+    );
+  });
+  return CombinationsSortedByKeys;
+};
+
 const ExplorePage: React.FC<ExplorePageProps> = ({ slug }) => {
-  if (slug === '') {
+  if (slug === "") {
     return <div>Slug is empty</div>;
   }
 
@@ -93,9 +129,19 @@ const ExplorePage: React.FC<ExplorePageProps> = ({ slug }) => {
     return <div>Component not found</div>;
   }
 
-  const Story = StoryData[component]['story'];
+  const Story = StoryData[component]["story"];
 
-  const StoryArgs = StoryData[component]['meta'];
+  const StoryArgs = StoryData[component]["meta"];
+  // Extract Meta info
+  let metaInfo = StoryArgs.metaInfo;
+  if (!metaInfo) {
+    metaInfo = {};
+  }
+  if (!metaInfo.clusteringOrder) {
+    metaInfo.clusteringOrder = ["size", "variant"];
+  }
+  // Extract Meta info
+
   const options: Options = StoryArgs.argTypes;
   const combinations: Combination[] = [];
   const allCombinations: Combination[] = [];
@@ -121,68 +167,224 @@ const ExplorePage: React.FC<ExplorePageProps> = ({ slug }) => {
   });
 
   let isStateComponent = false;
+  let clusterOrderSortedCombinations = groupAllSortedCombinations(
+    allCombinations,
+    metaInfo.clusteringOrder[0]
+  );
+
+  if (Object.keys(clusterOrderSortedCombinations).length === 0) {
+    clusterOrderSortedCombinations["default"] = allCombinations;
+    let varainatSortedCombinations = groupAllSortedCombinations(
+      clusterOrderSortedCombinations["default"],
+      metaInfo.clusteringOrder[1]
+    );
+    if (Object.keys(varainatSortedCombinations).length === 0) {
+      clusterOrderSortedCombinations["default"] = {};
+      clusterOrderSortedCombinations["default"]["default"] = allCombinations;
+    } else {
+      clusterOrderSortedCombinations["default"] = varainatSortedCombinations;
+    }
+  } else {
+    Object.keys(clusterOrderSortedCombinations).forEach((sizeKey) => {
+      let varainatSortedCombinations = groupAllSortedCombinations(
+        clusterOrderSortedCombinations[sizeKey],
+        metaInfo.clusteringOrder[1]
+      );
+      if (Object.keys(varainatSortedCombinations).length === 0) {
+        let tempSizeCombs = Array.isArray(
+          clusterOrderSortedCombinations[sizeKey]
+        )
+          ? [...clusterOrderSortedCombinations[sizeKey]]
+          : { ...clusterOrderSortedCombinations[sizeKey] };
+        clusterOrderSortedCombinations[sizeKey] = {};
+        clusterOrderSortedCombinations[sizeKey]["default"] = tempSizeCombs;
+      } else {
+        clusterOrderSortedCombinations[sizeKey] = varainatSortedCombinations;
+      }
+    });
+  }
+
+  // console.log("MetaInfo", clusterOrderSortedCombinations);
 
   return (
-    <Center p='$4' bg='$white'>
+    <Center p="$4" bg="$white" overflow="scroll" w="100%">
       <Heading
-        size='2xl'
+        size="2xl"
         my={50}
-        w='60%'
-        bg='$yellow300'
+        w="60%"
+        bg="$yellow300"
         py={16}
         px={32}
-        rounded='$md'
-        letterSpacing='$xl'
-        color='$textLight800'
+        rounded="$md"
+        letterSpacing="$xl"
+        color="$textLight800"
       >
         {component[0].toUpperCase()}
       </Heading>
       {allCombinations.length === 0 && (
         <Story
           dataSet={{
-            'component-props': JSON.stringify({
-              'component-name': component[0],
+            "component-props": JSON.stringify({
+              "component-name": component[0],
             }),
           }}
         />
       )}
       {allCombinations.length > 0 && (
-        <VStack p='$4' space='4xl'>
-          {allCombinations.map((props: any, index) => {
-            const dataProps: any = { ...props };
-            dataProps['component-name'] = component[0];
+        <VStack
+          p="$4"
+          space="4xl"
+          borderStyle="dashed"
+          borderWidth={"$1"}
+          borderColor="$teal400"
+          borderRadius="$2xl"
+        >
+          {Object.keys(clusterOrderSortedCombinations).map(
+            (clusterFirstOrder: any, index) => {
+              let variantSortedCombination =
+                clusterOrderSortedCombinations[clusterFirstOrder];
 
-            STATE_PROPERTIES.forEach((state) => {
-              if (props[state]) {
-                isStateComponent = true;
-                dataProps['state'] = state;
-                delete dataProps[state];
-              }
-            });
+              return (
+                <HStack
+                  space="3xl"
+                  p="$4"
+                  alignItems="flex-start"
+                  // justifyContent="center"
+                >
+                  {clusterFirstOrder === "default" ? (
+                    <></>
+                  ) : (
+                    <Heading>{clusterFirstOrder}</Heading>
+                  )}
+                  {Object.keys(variantSortedCombination).map(
+                    (variantName: any, index) => {
+                      let stateSortedCombination =
+                        variantSortedCombination[variantName];
 
-            if (!dataProps['state'] && isStateComponent) {
-              dataProps['state'] = 'default';
+                      return (
+                        <VStack space="md">
+                          {variantName === "default" ? (
+                            <></>
+                          ) : (
+                            <Heading bold>{variantName}</Heading>
+                          )}
+                          {/* <Text bold>{variantName}</Text> */}
+                          {variantName === "default" ? (
+                            <HStack
+                              w="$full"
+                              maxWidth="1200px"
+                              flexWrap="wrap"
+                              space="lg"
+                            >
+                              {Array.isArray(stateSortedCombination) &&
+                                stateSortedCombination.map((props: any) => {
+                                  props = {
+                                    ...props,
+                                    // orientation: clusterFirstOrder,
+                                    // clusterFirstOrder: variantName,
+                                  };
+                                  const dataProps: any = {
+                                    ...props,
+                                  };
+                                  dataProps["component-name"] = component[0];
+
+                                  STATE_PROPERTIES.forEach((state) => {
+                                    if (props[state]) {
+                                      isStateComponent = true;
+                                      dataProps["state"] = state;
+                                      delete dataProps[state];
+                                    }
+                                  });
+
+                                  if (!dataProps["state"] && isStateComponent) {
+                                    dataProps["state"] = "default";
+                                  }
+
+                                  if (
+                                    dataProps.uri &&
+                                    dataProps.uri === "https://broken.link"
+                                  ) {
+                                    dataProps.uri = "BrokenLink";
+                                  } else if (dataProps.uri) {
+                                    dataProps.uri = "ImageLink";
+                                  }
+
+                                  if (dataProps.name) {
+                                    dataProps.name = dataProps.name.displayName;
+                                  }
+
+                                  props.dataSet = {
+                                    "component-props":
+                                      JSON.stringify(dataProps),
+                                  };
+
+                                  return (
+                                    <Center key={index}>
+                                      <Story {...props} />
+                                    </Center>
+                                  );
+                                })}
+                            </HStack>
+                          ) : (
+                            <VStack space="lg">
+                              {Array.isArray(stateSortedCombination) &&
+                                stateSortedCombination.map((props: any) => {
+                                  props = {
+                                    ...props,
+                                    // clusterFirstOrder,
+                                    // variant: variantName,
+                                  };
+                                  const dataProps: any = {
+                                    ...props,
+                                  };
+                                  dataProps["component-name"] = component[0];
+
+                                  STATE_PROPERTIES.forEach((state) => {
+                                    if (props[state]) {
+                                      isStateComponent = true;
+                                      dataProps["state"] = state;
+                                      delete dataProps[state];
+                                    }
+                                  });
+
+                                  if (!dataProps["state"] && isStateComponent) {
+                                    dataProps["state"] = "default";
+                                  }
+
+                                  if (
+                                    dataProps.uri &&
+                                    dataProps.uri === "https://broken.link"
+                                  ) {
+                                    dataProps.uri = "BrokenLink";
+                                  } else if (dataProps.uri) {
+                                    dataProps.uri = "ImageLink";
+                                  }
+
+                                  if (dataProps.name) {
+                                    dataProps.name = dataProps.name.displayName;
+                                  }
+
+                                  props.dataSet = {
+                                    "component-props":
+                                      JSON.stringify(dataProps),
+                                  };
+
+                                  return (
+                                    <Center key={index}>
+                                      <Story {...props} />
+                                    </Center>
+                                  );
+                                })}
+                            </VStack>
+                          )}
+                        </VStack>
+                      );
+                    }
+                  )}
+                </HStack>
+              );
             }
-
-            if (dataProps.uri && dataProps.uri === 'https://broken.link') {
-              dataProps.uri = 'BrokenLink';
-            } else if (dataProps.uri) {
-              dataProps.uri = 'ImageLink';
-            }
-
-            if (dataProps.name) {
-              dataProps.name = dataProps.name.displayName;
-            }
-
-            props.dataSet = {
-              'component-props': JSON.stringify(dataProps),
-            };
-            return (
-              <Center key={index}>
-                <Story {...props} />
-              </Center>
-            );
-          })}
+          )}
         </VStack>
       )}
     </Center>
@@ -191,7 +393,7 @@ const ExplorePage: React.FC<ExplorePageProps> = ({ slug }) => {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const baseDirPath = process.cwd();
-  const tree = DirectoryTree(path.join(baseDirPath, 'components/stories'));
+  const tree = DirectoryTree(path.join(baseDirPath, "components/stories"));
   const filePaths = getFilePaths(tree);
   const paths: { params: { slug: string[] } }[] = [];
 
@@ -213,7 +415,7 @@ export const getStaticProps: GetStaticProps<ExplorePageProps> = async (
   const { slug } = context.params as { slug: string };
   return {
     props: {
-      slug: slug || '',
+      slug: slug || "",
     },
   };
 };
